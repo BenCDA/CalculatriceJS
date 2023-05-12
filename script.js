c1fd8c52177d900ec0a59657af3fc6a9ee75297e
BenCDA <cardosobenjamin01@gmail.com>
2023-05-12 13:16:31 +0200

    CalculatriceJS
    end project

---
+++ script.js
@@ -0,0 +1,39 @@
+// Manipulation du DOM, on récupère nos éléments
+const touches = [...document.querySelectorAll('.bouton')];
+const dataClavier = touches.map(touche => touche.dataset.clavier);
+const ecran = document.querySelector('.ecran');
+
+document.addEventListener('keydown', (e) => {
+    const value = e.key.toString();
+    calculer(value)
+})
+
+document.addEventListener('click', (e) => {
+    const value = e.target.dataset.clavier;
+    calculer(value)
+})
+
+//Fonction pour calculer, prend en paramètre la valeur du keycode e
+const calculer = (value) => {
+    if (dataClavier.includes(value)) {
+        switch(value) {
+            case '8':
+                ecran.textContent = "";
+                break;
+            case '13':
+                const calcul = eval(ecran.textContent);
+                ecran.textContent = calcul    
+                break;
+            default:
+                const indexClavier = dataClavier.indexOf(value);
+                const touche = touches[indexClavier];
+                ecran.textContent += touche.innerHTML;
+        }
+    }
+
+}
+
+// Gestion des erreurs
+window.addEventListener('error', (e) => {
+    alert('Une erreur est survenue : ' + e.message)
+})
